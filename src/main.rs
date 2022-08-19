9e5e0c2cb9b4d6333b9767c5377cef4bb414f0f4
carghai <carghai74@gmail.com>
2022-08-19 15:19:53 -0400

    works

--- src/main.rs
+++ src/main.rs
@@ -5,106 +5,19 @@ use rocket_contrib::json::Json;
 use rusqlite::Connection;
 use serde::Serialize;
 
-#[derive(Serialize)]
-struct ToDoList {
-    items: Vec<ToDoItem>,
-}
-
-#[derive(Serialize)]
-struct ToDoItem {
-    id: i64,
-    item: String,
-}
-
-#[derive(Serialize)]
-struct StatusMessage {
-    message: String,
-}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
-
-#[get("/todo")]
-fn fetch_all_todo_items() -> Result<Json<ToDoList>, String> {
-    let db_connection = match Connection::open("data.sqlite") {
-        Ok(connection) => connection,
-        Err(_) => {
-            return Err(String::from("Failed to connect to database"));
-        }
-    };
-
-    let mut statement = match db_connection.prepare("select id, item from todo_list;") {
-        Ok(statement) => statement,
-        Err(_) => return Err("Failed to prepare query".into()),
-    };
-
-    let results = statement.query_map(rusqlite::NO_PARAMS, |row| {
-        Ok(ToDoItem {
-            id: row.get(0)?,
-            item: row.get(1)?,
-        })
-    });
-
-    match results {
-        Ok(rows) => {
-            let collection: rusqlite::Result<Vec<_>> = rows.collect();
-
-            match collection {
-                Ok(items) => Ok(Json(ToDoList { items })),
-                Err(_) => Err("Could not collect items".into()),
-            }
-        }
-        Err(_) => Err("Failed to fetch todo items".into()),
-    }
+#[derive(Serialize)]
+struct DataPlaceHolder {
+    data : String,
 }
 
-#[post("/todo", format = "json", data = "<item>")]
-fn add_todo_item(item: Json<String>) -> Result<Json<StatusMessage>, String> {
-    let db_connection = match Connection::open("data.sqlite") {
-        Ok(connection) => connection,
-        Err(_) => {
-            return Err(String::from("Failed to connect to database"));
-        }
-    };
-
-    let mut statement =
-        match db_connection.prepare("insert into todo_list (id, item) values (null, $1);") {
-            Ok(statement) => statement,
-            Err(_) => return Err("Failed to prepare query".into()),
-        };
-    let results = statement.execute(&[&item.0]);
-
-    match results {
-        Ok(rows_affected) => Ok(Json(StatusMessage {
-            message: format!("{} rows inserted!", rows_affected),
-        })),
-        Err(_) => Err("Failed to insert todo item".into()),
-    }
-}
-
-#[delete("/todo/<id>")]
-fn remove_todo_item(id: i64) -> Result<Json<StatusMessage>, String> {
-    let db_connection = match Connection::open("data.sqlite") {
-        Ok(connection) => connection,
-        Err(_) => {
-            return Err(String::from("Failed to connect to database"));
-        }
-    };
-
-    let mut statement = match db_connection.prepare("delete from todo_list where id = $1;") {
-        Ok(statement) => statement,
-        Err(_) => return Err("Failed to prepare query".into()),
-    };
-    let results = statement.execute(&[&id]);
-
-    match results {
-        Ok(rows_affected) => Ok(Json(StatusMessage {
-            message: format!("{} rows deleted!", rows_affected),
-        })),
-        Err(_) => Err("Failed to delete todo item".into()),
-    }
+#[get("/data")]
+fn data_test() -> Json<DataPlaceHolder> {
+    Json(DataPlaceHolder { data : "yes".parse().unwrap() })
 }
 
 fn main() {
@@ -125,7 +38,7 @@ fn main() {
     rocket::ignite()
         .mount(
             "/",
-            routes![index, fetch_all_todo_items, add_todo_item, remove_todo_item],
+            routes![index, fetch_all_todo_items, add_todo_item, remove_todo_item,data_test],
         )
         .launch();
 }
