cfe4dc37efd5a5fc73a76041d969ae5b1d358f55
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-20 13:16:08 -0400

    fixes and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+//this is needed don't delete
 use rocket::*;
 
 mod routes;
@@ -11,3 +12,6 @@ fn main() {
         )
         .launch();
 }
+// rustup override set nightly
+// do this so night can be used in the code
+// this code will only work with nightly
