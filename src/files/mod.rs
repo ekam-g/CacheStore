25550d66ebeb7f23ecd4caef12da30bf0df3112c
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-23 12:30:41 -0400

    clean up and faster

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -3,53 +3,53 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Empty, empty, Write};
 
 
-pub(crate) struct Modify();
+pub struct Modify();
 
-impl Modify{
-   pub fn write(&self, data: String, path: &str) -> Result<Empty, io::Error> {
-        let  output = File::create(path);
-        match output {
+impl Modify {
+    pub fn write(&self, data: String, path: &str) -> Result<Empty, io::Error> {
+        let output = File::create(path);
+        return match output {
             Ok(mut file) => {
                 let error = write!(file, "{}", data);
                 match error {
                     Ok(..) => {
-                        return Ok(empty());
+                        Ok(empty())
                     }
                     Err(e) => {
-                     return Err(e);
+                        Err(e)
                     }
                 }
             }
             Err(e) => {
-                return Err(e);
+                Err(e)
             }
-        }
+        };
     }
 
-   pub fn read(&self, path: &str) -> Result<Vec<String>, io::Error>{
-       let file = File::open(path.to_string());
-       let mut v :Vec<String> = Vec::new();
-       match file {
-           Ok(success) => {
-               let reader = BufReader::new(success);
-               for line in reader.lines() {
-                   match line {
-                       Ok(l) => {
-                           v.push(l);
-                       }
-                       Err(e) => {
-                           v.push(e.to_string());
-                           return Err(e);
-                       }
-                   }
-               }
-           }
-           Err(error) => {
-               v.push(error.to_string());
-              return Err(error);
-           }
-       }
-       return Ok(v);
+    pub fn read(&self, path: &str) -> Result<Vec<String>, io::Error> {
+        let file = File::open(path.to_string());
+        let mut v: Vec<String> = Vec::new();
+        match file {
+            Ok(success) => {
+                let reader = BufReader::new(success);
+                for line in reader.lines() {
+                    match line {
+                        Ok(l) => {
+                            v.push(l);
+                        }
+                        Err(e) => {
+                            v.push(e.to_string());
+                            return Err(e);
+                        }
+                    }
+                }
+            }
+            Err(error) => {
+                v.push(error.to_string());
+                return Err(error);
+            }
+        }
+        return Ok(v);
     }
 }
 
