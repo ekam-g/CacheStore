c7df625e61ab4955aab01b0acf1a88b1ebf3f97b
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-29 13:25:40 -0400

    api key start

--- src/https/mod.rs
+++ src/https/mod.rs
@@ -5,10 +5,17 @@ use rocket::*;
 
 pub struct Web {}
 
+pub struct State {
+    api_key : String
+}
 
 impl Web {
     pub fn start(&self) {
+        let state: State  = State{
+            api_key : "gdfjshadgfa2432h4g51u4o5".to_string()
+        };
         ignite()
+        .manage(state)
             .mount(
                 "/",
                 routes![routes::index,routes::display_data::data_test, routes::add_data::add, routes::delete::delete],
