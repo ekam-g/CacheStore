cce5bb8ab0c3e29bce9cfea2bbf2bb200ac41a07
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-06 18:44:49 -0400

    changes add data

--- src/https/routes/add_data.rs
+++ src/https/routes/add_data.rs
@@ -43,15 +43,42 @@ pub fn add(
         });
     }
     let final_path = path_second(path);
-    let file_error =
-        txt_writer::WriteData {}.add(&data, format!("{}/{}.txt", &final_path, &data_name));
-    return match file_error {
-        Ok(_) => Json(Data {
-            error: "Success".to_string(),
-        }),
+    let file_error = txt_writer::ReadData {}.read(format!("{}/{}.txt", &final_path, &data_name));
+    match file_error {
+        Ok(read_data) => {
+            if read_data[0] != api_state.null {
+                let error = txt_writer::WriteData {}
+                    .drop_add(data, format!("{}/{}.txt", final_path, data_name));
+                match error {
+                    Ok(_) => {
+                        return Json(Data {
+                            error: "Success".to_string(),
+                        })
+                    }
+                    Err(e) => {
+                        return Json(Data {
+                            error: e.to_string(),
+                        })
+                    }
+                }
+            } else {
+                return AddDataFunc {}.make_file(data, final_path, data_name);
+            }
+        }
         Err(_) => {
-            let _ = better_file_maker::make_folders(&final_path);
-            AddDataFunc {}.make_file(data, final_path, data_name)
+            let writing_error = txt_writer::WriteData {}
+                .replace(&data, format!("{}/{}.txt", &final_path, &data_name));
+            match writing_error {
+                Err(_) => {
+                    let _ = better_file_maker::make_folders(&final_path);
+                    return AddDataFunc {}.make_file(data, final_path, data_name);
+                }
+                Ok(_) => {
+                    return Json(Data {
+                        error: "Success".to_string(),
+                    })
+                }
+            }
         }
     };
 }
