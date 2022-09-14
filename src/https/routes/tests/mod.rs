9b3389968366f940226384f7ed28c515b8c04bf3
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-14 16:50:11 -0400

    changed test

--- src/https/routes/tests/mod.rs
+++ src/https/routes/tests/mod.rs
@@ -22,32 +22,32 @@ mod test {
     async fn add_data_test() {
         test_url(
             "http://localhost:8000/add/test`worked/data/this is going very well/your_api_key"
-                .to_string(),
+                .to_owned(),
         )
         .await;
         test_url(
             "http://localhost:8000/add/test`worked/data/this is going very well/your_api_key"
-                .to_string(),
+                .to_owned(),
         )
         .await;
     }
     #[tokio::test]
     async fn read_data_test() {
         thread::sleep(time::Duration::from_secs(1));
-        test_url("http://localhost:8000/read/test`worked`data/your_api_key".to_string()).await;
+        test_url("http://localhost:8000/read/test`worked`data/your_api_key".to_owned()).await;
     }
     #[tokio::test]
     async fn read_data_test_many() {
         thread::sleep(time::Duration::from_secs(1));
         let data = http_request::Request::read_more(
-            "http://localhost:8000/read/test`worked`data/your_api_key".to_string(),
+            "http://localhost:8000/read/test`worked`data/your_api_key".to_owned(),
         )
         .await
         .expect("");
         if data.data
             != vec![
-                "this is going very well".to_string(),
-                "this is going very well".to_string(),
+                "this is going very well".to_owned(),
+                "this is going very well".to_owned(),
             ]
         {
             panic!("data does not equal correct amount")
@@ -56,13 +56,13 @@ mod test {
     #[tokio::test]
     async fn delete_data_test() {
         thread::sleep(time::Duration::from_secs(3));
-        test_url("http://localhost:8000/delete/test`worked`data/your_api_key".to_string()).await;
+        test_url("http://localhost:8000/delete/test`worked`data/your_api_key".to_owned()).await;
     }
     #[tokio::test]
     async fn delete_data_test_check() {
         thread::sleep(time::Duration::from_secs(4));
         let data = http_request::Request::read(
-            "http://localhost:8000/read/test`worked`data/your_api_key".to_string(),
+            "http://localhost:8000/read/test`worked`data/your_api_key".to_owned(),
         )
         .await
         .expect("");
@@ -73,23 +73,22 @@ mod test {
     #[tokio::test]
     async fn null_test() {
         thread::sleep(time::Duration::from_secs(5));
-        test_url("http://localhost:8000/null_write/test`worked`data/your_api_key".to_string())
-            .await;
+        test_url("http://localhost:8000/null_write/test`worked`data/your_api_key".to_owned()).await;
     }
     #[tokio::test]
     async fn null_test_check() {
         thread::sleep(time::Duration::from_secs(6));
         test_url(
             "http://localhost:8000/add/test`worked/data/this is going very well/your_api_key"
-                .to_string(),
+                .to_owned(),
         )
         .await;
     }
     #[tokio::test]
     async fn read_data_test_null() {
         thread::sleep(time::Duration::from_secs(7));
-        test_url("http://localhost:8000/read/test`worked`data/your_api_key".to_string()).await;
-        test_url("http://localhost:8000/delete/test`worked`data/your_api_key".to_string()).await;
+        test_url("http://localhost:8000/read/test`worked`data/your_api_key".to_owned()).await;
+        test_url("http://localhost:8000/delete/test`worked`data/your_api_key".to_owned()).await;
     }
     // Local functions testing
     //_________________________________________________________________________________________________________________________
@@ -97,7 +96,7 @@ mod test {
         let check_data = func.read_data("test/worked/local/data");
         match check_data {
             Ok(data) => {
-                panic!("{}", data[0].to_string())
+                panic!("{}", data[0].to_owned())
             }
             Err(e) => {
                 if e != what_error {
@@ -110,9 +109,9 @@ mod test {
     #[test]
     fn add_data_test_local() {
         let func = StateData {
-            api_key: "your_api_key".to_string(),
-            null: "null_nil_value_key:345n,234lj52".to_string(),
-            data_storage_location: "database/".to_string(),
+            api_key: "your_api_key".to_owned(),
+            null: "null_nil_value_key:345n,234lj52".to_owned(),
+            data_storage_location: "database/".to_owned(),
         };
         func.write_data("this is going very well", "test/worked/local", "data")
             .expect("failed when writing data");
@@ -123,17 +122,17 @@ mod test {
     fn read_data_test_local() {
         thread::sleep(time::Duration::from_secs(1));
         let func = StateData {
-            api_key: "your_api_key".to_string(),
-            null: "null_nil_value_key:345n,234lj52".to_string(),
-            data_storage_location: "database/".to_string(),
+            api_key: "your_api_key".to_owned(),
+            null: "null_nil_value_key:345n,234lj52".to_owned(),
+            data_storage_location: "database/".to_owned(),
         };
         let check_data = func
             .read_data("test/worked/local/data")
             .expect("failed when reading");
         if check_data
             != vec![
-                "this is going very well".to_string(),
-                "this is going very well".to_string(),
+                "this is going very well".to_owned(),
+                "this is going very well".to_owned(),
             ]
         {
             panic!("write data did not work!")
@@ -143,9 +142,9 @@ mod test {
     fn null_test_check_local() {
         thread::sleep(time::Duration::from_secs(2));
         let func = StateData {
-            api_key: "your_api_key".to_string(),
-            null: "null_nil_value_key:345n,234lj52".to_string(),
-            data_storage_location: "database/".to_string(),
+            api_key: "your_api_key".to_owned(),
+            null: "null_nil_value_key:345n,234lj52".to_owned(),
+            data_storage_location: "database/".to_owned(),
         };
         func.null_write("test/worked/local/data")
             .expect("write null functions failed");
@@ -157,9 +156,9 @@ mod test {
     fn delete_data_test_check_local() {
         thread::sleep(time::Duration::from_secs(3));
         let func = StateData {
-            api_key: "your_api_key".to_string(),
-            null: "null_nil_value_key:345n,234lj52".to_string(),
-            data_storage_location: "database/".to_string(),
+            api_key: "your_api_key".to_owned(),
+            null: "null_nil_value_key:345n,234lj52".to_owned(),
+            data_storage_location: "database/".to_owned(),
         };
         func.delete_data("test/worked/local/data")
             .expect("delete failed");
